592109cd6c0cc3a2d064d7e9257a38a0dcff120b
deep1757 <71060144+deep1757@users.noreply.github.com>
2021-07-29 13:04:08 +0530

    Update fibonnaci string.py

--- fibonnaci string.py
+++ fibonnaci string.py
@@ -1,11 +1,15 @@
 from collections import Counter
+# for loop to enter input value 
 for i in range(int(input())):
     o = input()
+    #create list to enter values 
     c = []
+    #create counter to values 
     d = dict(Counter(o))
     for e in d.values():
         c.append(e)
     c.sort()
+    #if else condition to check values
     if len(c)<3:
         print("Dynamic")
     else:
